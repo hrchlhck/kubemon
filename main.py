624dfedc3924cd9279c5e150a16b1a37615d5bfa
hrchlhck <vpemfh7@gmail.com>
2020-09-09 11:28:01 -0300

    Added feature to read args

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from sys_monitor.monitor import Monitor
-
-
-def main():
-    try:
-        monitor = Monitor()
-        monitor.start()
-    except KeyboardInterrupt:
-        print('\nExiting')
-
+from requests import exceptions
+from sys_monitor.cli.commands import *
+import sys
 
 if __name__ == "__main__":
-    main()
+    """
+        args[2] -> ip
+        args[3] -> port
+    """
+    args = sys.argv
+    
+    if len(args) > 1:
+        if args[1] == 'collector':
+            start_collector(args[2], args[3])
+        elif args[1] == 'monitor':
+            try:
+                start_monitor(args[2], args[3])
+            except exceptions.ConnectionError:
+                print('Connection refused')
+        else:
+            print('Option doesn\'t exist')
+    else:
+        print('No option selected')
+    
