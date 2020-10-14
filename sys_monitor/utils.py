30f5822cfbda8da66440e4e9b04e6e46b35f3a20
hrchlhck <vpemfh7@gmail.com>
2020-10-14 17:57:10 -0300

    Added utility functions

---
+++ sys_monitor/utils.py
@@ -0,0 +1,52 @@
+from functools import reduce
+from requests import get
+from operator import sub
+import json
+
+def subtract_dicts(dict1, dict2):
+    """ Subtracts values from dict1 and dict2 """
+    if len(dict1) != len(dict2):
+        raise KeyError("Mapping key not found")
+    values = list(map(lambda _dict: reduce(sub, _dict),
+                      zip(dict2.values(), dict1.values())))
+    return dict(zip(dict1.keys(), values))
+
+
+def merge_dict(*dicts):
+    """ Merges multiple dictionaries """
+    result = {}
+    for d in dicts:
+        if d != None:
+          result.update(d)
+    return result
+
+
+def filter_dict(_dict, *keys):
+    """ Apply a simple filter over a given dictionary
+        Usage:
+            >>> filter_dict({'a': 1, 'b': 2, 'c':3}, 'a', 'c')
+            >>> {'a': 1, 'c': 3}
+    """
+    filters = keys
+    if isinstance(keys[0], list):
+        filters = keys[0]
+    return {k: v for k, v in _dict.items() if k in filters}
+
+
+def join(url, *pages):
+    """ Joins pages in a given URL. 
+        Usage:
+            >>> join('https://github.com', 'hrchlhck', 'sys-monitor')
+            >>> 'https://github.com/hrchlhck/sys-monitor'
+    """
+    for page in map(str, pages):
+        url += "/" + page
+    return url
+
+
+def load_json(url):
+    """ Parses a JSON to a Python dictionary """
+    try:
+        return json.loads(get(url).text)
+    except:
+        return []
