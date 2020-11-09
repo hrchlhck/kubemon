6f6d18239c86a23f9b3b19c8e9b1ea40a4f7c47a
hrchlhck <vpemfh7@gmail.com>
2020-11-09 13:20:49 -0300

    Added merge command and setted visibility for command functions only

--- sys_monitor/cli/commands.py
+++ sys_monitor/cli/commands.py
@@ -1,7 +1,9 @@
 from ..monitor import Monitor
 from ..collector import Collector
 from ..spark_monitor import SparkMonitor
+from ..merge import merge
 
+__all__ = ["start_monitor", "start_collector", "start_spark_monitor", "merge_files"]
 
 def start_monitor(addr="localhost", port=9822):
     m = Monitor(addr, int(port), verbose=True)
@@ -16,3 +18,7 @@ def start_collector(addr="0.0.0.0", port=9822):
 def start_spark_monitor(addr, port=9822):
     sm = SparkMonitor(addr, port)
     sm.start()
+
+
+def merge_files(file1, file2):
+    merge(file1, file2)
