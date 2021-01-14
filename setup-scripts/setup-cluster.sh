ced6fdb234dde0caabe6d2e1aebd84499900d4bc
hrchlhck <vpemfh7@gmail.com>
2021-01-14 16:51:18 -0300

    Commented code and fixed #4 and added #5

--- setup-scripts/setup-cluster.sh
+++ setup-scripts/setup-cluster.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-CLUSTER_IP=`ip route get 1 | awk {'print $7;exit'}`
-kbcfg="$HOME/.kube/config"
+### Resets cluster and some garbage
+sudo kubeadm reset <<< y # Accept reset
+sudo rm -rf $HOME/.kube
+sudo rm -rf /etc/cni/net.d
 
-kubeadm reset
-rm -rf $HOME/.kube
-rm -rf /etc/cni/net.d
+### Initiate Kubernetes master
+sudo kubeadm init --pod-network-cidr=10.0.1.0/24 
 
-kubeadm init --pod-network-cidr=10.244.0.0/24 
-
+### Allows to execute kubectl as a regular user
 mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-echo "KUBECONFIG=${kbcfg}" >> $HOME/.bashrc
+### Saves the config to bashrc and reload it
+if [[ ! $(grep -R "KUBECONFIG" $HOME/.bashrc) ]]; then
+    echo "KUBECONFIG=${HOME}/.kube/config" >> $HOME/.bashrc
+    . $HOME/.bashrc
+fi
 
-export KUEBCONFIG=kbcfg
+### Apply Weave Net CNI
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')&env.IPALLOC_RANGE=10.0.1.0/24"
 
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')&env.IPALLOC_RANGE=10.244.0.0/24"
-
-kubectl taint nodes $(hostname) node-role.kubernetes.io/master-
+### Untaint master node 
+# kubectl taint nodes $(hostname) node-role.kubernetes.io/master-
