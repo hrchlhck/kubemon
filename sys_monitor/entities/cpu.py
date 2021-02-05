c647235ebdccebc7c711f2cc464fb0c507dfb46d
hrchlhck <vpemfh7@gmail.com>
2021-02-05 17:14:02 -0300

    Collecting more data about CPU and added tests

--- sys_monitor/entities/cpu.py
+++ sys_monitor/entities/cpu.py
@@ -1,19 +1,33 @@
-from psutil import cpu_times as cpu
-from psutil import getloadavg as loadavg
+from psutil import cpu_times, cpu_stats
+from addict import Dict
 from .base_entity import BaseEntity
 
 
 class CPU(BaseEntity):
     """ Represents an CPU object that returns CPU usage of a system """
-    def __get_busy_time(self):
-        c = cpu()
-        return c.user + c.system + c.irq + c.softirq + c.nice 
+
+    def __init__(self):
+        self.__infos = Dict()
+
+    def __set_data(self):
+        # Cpu stats
+        self.__infos.cpu_stat_ctx_switches = cpu_stats().ctx_switches
+        self.__infos.cpu_stat_interrupts = cpu_stats().interrupts
+        self.__infos.cpu_stat_soft_interrupts = cpu_stats().soft_interrupts
+        self.__infos.cpu_stat_syscalls = cpu_stats().syscalls
+
+        # Cpu times
+        self.__infos.cpu_times_user = cpu_times().user
+        self.__infos.cpu_times_system = cpu_times().system
+        self.__infos.cpu_times_nice = cpu_times().nice
+        self.__infos.cpu_times_softirq = cpu_times().softirq
+        self.__infos.cpu_times_irq = cpu_times().irq
+        self.__infos.cpu_times_iowait = cpu_times().iowait
+        self.__infos.cpu_times_guest = cpu_times().guest
+        self.__infos.cpu_times_guest_nice = cpu_times().guest_nice
+        self.__infos.cpu_times_idle = cpu_times().idle
 
     @property
     def get_usage(self):
-        return round(self.__get_busy_time(), 4)
-    
-    @property
-    def get_loadavg(self):
-        return loadavg()
-
+        self.__set_data()
+        return self.__infos
--- tests/resources/test_cpu.py
+++ tests/resources/test_cpu.py
@@ -1,5 +1,5 @@
 from sys_monitor import CPU
-from psutil import cpu_times
+from psutil import cpu_times, cpu_stats
 from time import sleep
 import unittest
 
@@ -7,27 +7,68 @@ import unittest
 class TestCPU(unittest.TestCase):
     def setUp(self):
         self.cpu = CPU()
+        self.infos = ['cpu_stat_ctx_switches',
+            'cpu_stat_interrupts',
+            'cpu_stat_soft_interrupts',
+            'cpu_stat_syscalls',
+            'cpu_times_user',
+            'cpu_times_system',
+            'cpu_times_nice',
+            'cpu_times_softirq',
+            'cpu_times_irq',
+            'cpu_times_iowait',
+            'cpu_times_guest',
+            'cpu_times_guest_nice',
+            'cpu_times_idle']
+        self.cpu_stats = cpu_stats()
+        self.cpu_times = cpu_times()
 
-    def get_load_avg(self):
-        with open('/proc/loadavg', mode='r') as f:
-            data = f.readlines()[0].split()
-            data = data[:3]
-            data = tuple(map(float, data))
-        return data
+    def test_sizes(self):
+        self.assertEqual(len(self.cpu.get_usage), len(self.infos))
 
-    def test_busy_time(self):
-        c = cpu_times()
-        c = c.user + c.system + c.irq + c.softirq + c.nice 
-        self.assertEqual(c, self.cpu.get_usage)
+    def test_cpu_info_keys(self):
+        errors = 0
+        for k in self.infos:
+            if k not in self.cpu.get_usage.keys():
+                errors += 1
+        self.assertEqual(errors, 0)
+    
+    def test_cpu_ctx_switches(self):
+        self.assertEqual(self.cpu.get_usage.cpu_stat_ctx_switches, self.cpu_stats.ctx_switches)
 
-    def test_busy_time_with_sleep(self):
-        c = cpu_times()
-        c = c.user + c.system + c.irq + c.softirq + c.nice 
-        sleep(5)
-        self.assertNotEqual(round(c, 4), self.cpu.get_usage)
+    def test_cpu_interrupts(self):
+        self.assertEqual(self.cpu.get_usage.cpu_stat_interrupts, self.cpu_stats.interrupts)
 
-    def test_load_avg(self):
-        self.assertEqual(self.cpu.get_loadavg, self.get_load_avg())
+    def test_cpu_soft_interrupts(self):
+        self.assertEqual(self.cpu.get_usage.cpu_stat_soft_interrupts, self.cpu_stats.soft_interrupts)
+
+    def test_cpu_syscalls(self):
+        self.assertEqual(self.cpu.get_usage.cpu_stat_syscalls, self.cpu_stats.syscalls)
+
+    def test_cpu_time_system(self):
+        self.assertEqual(self.cpu.get_usage.cpu_times_system, self.cpu_times.system)
+
+    def test_cpu_time_idle(self):
+        self.assertEqual(self.cpu.get_usage.cpu_times_idle, self.cpu_times.idle)
+
+    def test_cpu_time_user(self):
+        self.assertEqual(self.cpu.get_usage.cpu_times_user, self.cpu_times.user)
+
+    def test_cpu_time_guest(self):
+        self.assertEqual(self.cpu.get_usage.cpu_times_guest, self.cpu_times.guest)
+
+    def test_cpu_time_iowait(self):
+        self.assertEqual(self.cpu.get_usage.cpu_times_iowait, self.cpu_times.iowait)
+
+    def test_cpu_time_irq(self):
+        self.assertEqual(self.cpu.get_usage.cpu_times_irq, self.cpu_times.irq)
+
+    def test_cpu_time_softirq(self):
+        self.assertEqual(self.cpu.get_usage.cpu_times_softirq, self.cpu_times.softirq)
+
+    def test_cpu_time_nice(self):
+        self.assertEqual(self.cpu.get_usage.cpu_times_nice, self.cpu_times.nice)
+
 
 if __name__ == '__main__':
     unittest.main()
