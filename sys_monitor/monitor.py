2d39788adba4e7ca59ebd0173a6ebffbfb1ca837
hrchlhck <vpemfh7@gmail.com>
2021-02-17 14:06:00 -0300

    Changed code to use function from utils.py

--- sys_monitor/monitor.py
+++ sys_monitor/monitor.py
@@ -2,7 +2,8 @@ from .entities.cpu import CPU
 from .entities.disk import Disk
 from .entities.network import Network
 from socket import socket, AF_INET, SOCK_STREAM, SO_REUSEADDR, SOL_SOCKET
-from .utils import subtract_dicts, send_data, CONNECTION_DIED_CODE
+from .utils import subtract_dicts, send, receive
+from .constants import CONNECTION_DIED_CODE
 from requests import get
 from time import sleep
 import psutil
@@ -33,39 +34,14 @@ class Monitor:
         
         return data
 
-    def __calc_usage(self):
+    def __calc_usage(self, interval):
         data = self.__get_data()
 
-        sleep(self.__interval)
+        sleep(interval)
         
         data_new = self.__get_data()
         
         return subtract_dicts(data, data_new)
 
     def start(self):
-        if not self.__verbose:
-            print("Running on silent mode\n")
-
-        with socket(AF_INET, SOCK_STREAM) as _socket:
-            buffer_size = 1024
-            _socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-            _socket.connect((self.__address, self.__port))
-            print("Connected monitor to collector")
-            
-            signal = _socket.recv(buffer_size).decode("utf8")
-
-            if signal and signal == "start":
-                print("Starting monitor")
-
-                try:
-                    while True:
-                        data = self.__calc_usage()
-                        
-                        if self.__verbose:
-                            print(data)
-
-                        send_data(_socket, data, "sys_wide_monitor")
-                        print(_socket.recv(buffer_size).decode("utf8"))
-                except:
-                    send_data(_socket, CONNECTION_DIED_CODE, "sys_wide_monitor")
-                    _socket.close()
+        send(self.__address, self.__port, self.__calc_usage, self.__interval, "sys")
