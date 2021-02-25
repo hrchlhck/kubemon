76b1c8c92d0273e65b3fbf77530fe877eb93013d
hrchlhck <vpemfh7@gmail.com>
2021-02-25 10:37:41 -0300

    Added defaults

--- sys_monitor/cli/commands.py
+++ sys_monitor/cli/commands.py
@@ -17,16 +17,14 @@ def get_system(sys_type, args):
             return ProcessMonitor(args.host, int(args.port))
         elif sys_type == 'docker':
             return DockerMonitor(args.host, int(args.port))
-    elif not args.port:
-        print("Missing --port/-p PORT")
     elif not args.host:
         print("Missing --host/-H IP")
 
 parser = argparse.ArgumentParser(description='sys-monitor commands')
 
 parser.add_argument('-t', '--type', help='Functionality of sys-monitor. E.g. collector, monitor, merge...')
-parser.add_argument('-H', '--host', help='Host that any of sys-monitor functions will be connecting', metavar='IP')
-parser.add_argument('-p', '--port', help='Port of the host')
+parser.add_argument('-H', '--host', default='0.0.0.0', help='Host that any of sys-monitor functions will be connecting', metavar='IP')
+parser.add_argument('-p', '--port', default=9822, help='Port of the host')
 parser.add_argument('-f', '--files', nargs=2, help='Files for merge', metavar=('FILE1', 'FILE2'))
 parser.add_argument('-mn', '--monitors',  default=3, help='Number of monitors that `collector` will be collecting data. By default, it`s 3')
 
