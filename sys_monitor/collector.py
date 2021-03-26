1aae38869e045fe4a2c71a09859afcae00320db7
hrchlhck <vpemfh7@gmail.com>
2021-03-26 20:56:01 -0300

    Added colors and fixed try/except

--- sys_monitor/collector.py
+++ sys_monitor/collector.py
@@ -3,7 +3,9 @@ from .utils import save_csv, receive, send_to
 from addict import Dict
 from datetime import datetime
 from collections import deque
+from sty import fg, rs, ef
 import socket
+import traceback
 import threading
 import sys
 
@@ -26,6 +28,7 @@ class Collector(object):
         self.__cli_port = cli_port
         self.__mutex = threading.Lock()
         self.__instances = deque()
+        self.__tag = f"[ {fg(255, 200, 100)}{self.__class__.__name__}{fg.rs} ] "
 
     @property
     def address(self):
@@ -55,7 +58,7 @@ class Collector(object):
             self.__instances.append(client)
             self.mutex.release()
 
-            print("\t[+] {}:{} connected".format(*address))
+            print("\t[ {}+{} ] {}:{} connected".format(fg(0, 255, 0), fg.rs, *address), flush=True)
 
             start_thread(self.__listen_to_client, (client, address))
   
@@ -63,8 +66,8 @@ class Collector(object):
         while True:
             data, addr = receive(client)
 
+            print("\t[ {}*{} ] Command '{}' received from {}:{}".format(fg(0, 90, 255), fg.rs, data, *addr), flush=True)
             if data:
-                print("\t[*] Command '{}' received from {}:{}".format(data, *addr))
                 if data == "/start":
                     if self.connected_instances == 0:
                         message = f"There are no connected monitors to be started"
@@ -81,7 +84,7 @@ class Collector(object):
     def __start_instances(self):
         for client in self.__instances:
             send_to(client, START_MESSAGE)
-            print(f"Started client {client}")
+            print(f"{self.__tag}Started client {client}", flush=True)
 
     def __setup_socket(self, address: str, port: int, socktype: socket.SocketKind) -> socket.socket:
         """ 
@@ -96,20 +99,20 @@ class Collector(object):
             sockfd.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             sockfd.bind((address, port))
         except:
-            print(f"Error while trying to bind socket to port {port}")
+            print(f"{self.__tag}Error while trying to bind socket to port {port}", flush=True)
             sockfd.close()
             exit(1)
         return sockfd
 
     def __start_cli(self):
         sockfd = self.__setup_socket(self.address, self.cli_port, socket.SOCK_DGRAM)
-        print(f"Started collector CLI at {self.address}:{self.cli_port}")
+        print(f"{self.__tag}Started collector CLI at {self.address}:{self.cli_port}", flush=True)
         self.__listen_cli(sockfd)
 
     def __start_collector(self):
         sockfd = self.__setup_socket(self.address, self.port, socket.SOCK_STREAM)
         sockfd.listen()
-        print(f"Started collector at {self.address}:{self.port}")
+        print(f"{self.__tag}Started collector at {self.address}:{self.port}", flush=True)
         self.__accept_connections(sockfd)
     
     def start(self):
@@ -117,23 +120,23 @@ class Collector(object):
         start_thread(self.__start_collector)
 
     def __listen_to_client(self, client: socket.socket, address: tuple) -> None:
-        print("Creating new thread for client {}:{}".format(*address))
+        print("{}Creating new thread for client {}:{}".format(self.__tag, *address), flush=True)
 
         while True:
             try:
-                data = receive(client, buffer_size=2048)
+                data, _ = receive(client, buffer_size=2048)
 
-                print("Received {} from {}:{}".format(data, *address))
+                print("{}Received {} from {}:{}".format(self.__tag, data, *address), flush=True)
 
                 if isinstance(data, dict):
                     data = Dict(data)
-                    file_name = f"{data.source}_{address[0].replace('.', '_')}_{address[1]}"
-                    save_csv(data.data, file_name, dir_name=data.source.split('_')[0])
+                    save_csv(data.data, data.source, dir_name=data.source.split('_')[0])
 
                 send_to(client, f"OK - {datetime.now()}")
 
-            except (ConnectionAbortedError, ConnectionResetError, EOFError) as e:
-                print(f"\t[-] {client} has died because {e}")
+            except:
+                traceback.print_exc()
+                print(f"\t[ {fg(255, 0, 0)}- {fg.rs}] {client}", flush=True)
                 self.mutex.acquire()
                 self.__instances.remove(client)
                 self.mutex.release()
