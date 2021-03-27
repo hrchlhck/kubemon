d47a8fd2aae1b47613ad99593ef5384695f2a1a6
hrchlhck <vpemfh7@gmail.com>
2021-03-26 21:15:28 -0300

    Added method to standardize monitor instances source names and fixed send() args

--- sys_monitor/base_monitor.py
+++ sys_monitor/base_monitor.py
@@ -4,7 +4,7 @@ from .exceptions import PidNotExistException
 from .decorators import wrap_exceptions
 from .constants import START_MESSAGE
 from typing import Callable
-from socket import AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR, socket
+from socket import AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR, socket, gethostname, gethostbyname
 from threading import Thread
 import os
 
@@ -47,8 +47,10 @@ class BaseMonitor(object):
         if not pid:
             fields = ['nr_active_file', 'nr_inactive_file', 'nr_mapped', 'nr_active_anon', 'nr_inactive_anon', 'pgpgin', 'pgpgout', 'pgfree', 'pgfault', 'pgmajfault', 'pgreuse']
             
-            def to_dict(nested_lists): return {k: int(v) for k, v in map(
-                lambda atom_list: atom_list.split(), nested_lists)}
+            def to_dict(nested_lists):
+                atoms = map(lambda atom_list: atom_list.split(), nested_lists)
+                ret = {k: int(v) for k, v in atoms}
+                return ret
 
             with open("/proc/vmstat", mode="r") as fd:
                 ret = to_dict(fd.readlines())
@@ -61,43 +63,63 @@ class BaseMonitor(object):
                 ret = {k: int(v) for k, v in zip(infos, ret.split())}
         return ret
 
-    @wrap_exceptions(KeyboardInterrupt, EOFError)
-    def send(self, function: Callable, function_args: list, _from="", container_name="", pid=0) -> None:
+    def get_source_name(self, pid=0, container_name=""):
+        """ 
+            Returns a standardized name to be sended to the collector instance
+
+            Standard: 
+                if monitor instance is OSMonitor, then
+                    (Monitor instance)_(IP address)_(Hostname)_(PID = 0)
+                else
+                    (Monitor instance)_(IP address)_(Hostname)_(Container name)_(PID)
+
+            Args:
+                pid (int): Represents the PID of a process or docker container
+                container_name (str): Represents the name of the process or docker container
+        """
+        parse_ip = lambda ip: ip.replace(".", "_")
+        ip = parse_ip(gethostbyname(gethostname()))
+        ret = f"{self.name}_{ip}_{gethostname()}_{pid}"
+
+        if self.name == "ProcessMonitor" or self.name == "DockerMonitor":
+            ret = f"{self.name}_{ip}_{gethostname()}_{container_name}_{pid}"
+
+        return ret
+
+    @wrap_exceptions(KeyboardInterrupt)
+    def send(self, function: Callable, function_args: list, container_name="", pid=0) -> None:
         """ 
         Wrapper function for gathering and sending data from docker containers in a gap of N seconds defined by `interval` parameter.
 
         Args:
             function (Callable): The function that will be gathering information
             function_args (list): The arguments of the `function` parameter
-            _from (str): Name where the data is being sent
             container_name (str): Name of the container
             pid (int): If it's not None, it will specify a PID for monitoring and gathering data
         """
+
+        source_name = self.get_source_name(pid=pid, container_name=container_name)
+
         with socket(AF_INET, SOCK_STREAM) as sock:
             sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-            try_connect(self.address, self.port, sock, self.interval)
+            sock.connect((self.address, self.port))
+            
+            print(f"Connected {source_name} collector to server", flush=True)
 
-            print("Connected %s collector to server" % format_name(_from))
-
-            signal = receive(sock)
+            signal, _ = receive(sock)
 
             if signal == START_MESSAGE:
-                print("Starting")
+                print(f"Starting {self.name}", flush=True)
 
                 while True:
                     ret = function(*function_args)
 
-                    print(ret)
+                    message = {"source": source_name, "data": ret}
 
-                    source = f"{_from}_{format_name(container_name)}_{pid}"
+                    send_to(sock, message)
 
-                    print(source)
-
-                    message = {"source": str(source), "data": dict(ret)}
-
-                    send_to(sock, message, to_group=False)
-
-                    print(receive(sock))
+                    recv, _ = receive(sock)
+                    print(recv, flush=True)
 
     def collect(self):
         """ Method to be implemented by child classes """
@@ -106,19 +128,17 @@ class BaseMonitor(object):
         class_name = self.name
 
         if "OSMonitor" == class_name:
-            self.send(self.collect, [self.interval], _from=class_name)
+            self.send(function=self.collect, function_args=[self.interval])
         elif "ProcessMonitor" == class_name:
             client = docker.from_env()
             containers = get_containers(client)
-            container_pids = [(c.name, get_container_pid(c))
-                              for c in containers]
+            container_pids = [(c.name, get_container_pid(c)) for c in containers]
             for container_name, pid in container_pids:
                 t = Thread(target=self.collect, args=(container_name, pid))
                 t.start()
         elif "DockerMonitor" == class_name:
-            pods = self.pods
-            for pod, containers in pods.items():
-                for container in containers:
+            for pod in self.pods:
+                for c in pod.containers:
                     t = Thread(target=self.collect, kwargs={
-                               'container': container, 'pod': pod})
+                               'container': c, 'pod': pod})
                     t.start()
