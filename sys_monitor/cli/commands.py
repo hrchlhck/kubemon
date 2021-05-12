df70b7eef5b826b04d46b35b3f7d0302e5205d1f
hrchlhck <vpemfh7@protonmail.com>
2021-05-12 18:04:22 -0300

    Added interval argument

--- sys_monitor/cli/commands.py
+++ sys_monitor/cli/commands.py
@@ -10,11 +10,11 @@ def get_system(sys_type, args):
         if sys_type == 'collector':
             return Collector(address=args.host, port=int(args.port))
         elif sys_type == 'monitor':
-            return OSMonitor(address=args.host, port=int(args.port))
+            return OSMonitor(address=args.host, port=int(args.port), interval=int(args.interval))
         elif sys_type == 'process':
-            return ProcessMonitor(address=args.host, port=int(args.port))
+            return ProcessMonitor(address=args.host, port=int(args.port), interval=int(args.interval))
         elif sys_type == 'docker':
-            return DockerMonitor(address=args.host, port=int(args.port))
+            return DockerMonitor(address=args.host, port=int(args.port), interval=int(args.interval))
         elif sys_type == 'cli':
             return CollectorClient(address=args.host, port=int(args.port))
     elif not args.host:
@@ -27,5 +27,6 @@ parser.add_argument('-H', '--host', default='0.0.0.0', help='Host that any of sy
 parser.add_argument('-p', '--port', default=9822, help='Port of the host')
 parser.add_argument('-f', '--files', nargs=2, help='Files for merge', metavar=('FILE1', 'FILE2'))
 parser.add_argument('-c', '--command', default="", nargs='*', help="Command for be executing on CollectorClient")
+parser.add_argument('-i', '--interval', default=5, help="Data collection rate by monitors")
 
 args = parser.parse_args()
