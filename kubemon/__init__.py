74f3c44e9845ab4dea30684f963f8bee38429295
hrchlhck <vpemfh7@protonmail.com>
2021-07-12 13:44:48 -0300

    Added module

--- kubemon/__init__.py
+++ kubemon/__init__.py
@@ -1,7 +1,7 @@
 from .entities.disk import Disk
 from .entities.network import Network
 from .entities.cpu import CPU
-from .collector import Collector
+from .collector import Collector, CollectorClient
 from .monitors import OSMonitor, DockerMonitor
 
-__all__ = ['Disk', 'Network', 'CPU', 'Collector', 'OSMonitor', 'DockerMonitor']
+__all__ = ['Disk', 'Network', 'CPU', 'Collector', 'OSMonitor', 'DockerMonitor', 'CollectorClient']
