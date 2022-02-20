373626672c45cd9baae7c4c358cf4bb8e1169187
hrchlhck <vpemfh7@protonmail.com>
2022-02-20 10:59:33 -0300

    Moved functions

--- kubemon/decorators.py
+++ kubemon/decorators.py
@@ -1,3 +1,4 @@
+from typing import Callable, Any
 from functools import wraps
 
 def wrap_exceptions(*exceptions):
@@ -15,5 +16,17 @@ def wrap_exceptions(*exceptions):
             except exceptions as e:
                 print(e)
                 exit(-1)
+        return wrapper
+    return decorator
+
+def _label_metrics(preffix: str, metrics: dict) -> dict:
+    return {f'{preffix}_{k}': v for k, v in metrics.items()}
+
+def label(preffix: str) -> Callable:
+    def decorator(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args, **kwargs) -> Any:
+            ret = func(*args, **kwargs)
+            return _label_metrics(preffix, ret)
         return wrapper
     return decorator
